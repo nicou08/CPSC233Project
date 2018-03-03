1010f808c78f66dc2dc459947c666a4669245389
Stephen Dios <stephendios@gmail.com>
2018-03-02 23:02:00 -0700

    Created Police sprites. Set police image in Police class

--- src/etphoneshome/entities/enemies/Police.java
+++ src/etphoneshome/entities/enemies/Police.java
@@ -4,4 +4,9 @@ package etphoneshome.entities.enemies;
  * This class is used for the Police type enemy. It is a derived class from the {@code Enemy} class
  */
 public class Police extends Enemy {
+
+    public Police()
+    {
+        this.setEntitySprite("images/sprites/police_sprite_left.png");
+    }
 }
